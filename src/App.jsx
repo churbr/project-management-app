e4133c82f73235d99d38caf8ce8a9153d55c9399
Bryan Chu <chu.reybr@gmail.com>
2024-06-10 02:48:55 +0800

    Handling Project Creation & Updating the UI

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ function App() {
     setProjectState((prevState) => {
       return {
         ...prevState,
+        projectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
@@ -44,7 +45,10 @@ function App() {
 
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
       {content}
     </main>
   );
