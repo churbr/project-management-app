6ebd73e5b502b2e550ad5c1fc4ee2c42140ee680
Bryan Chu <chu.reybr@gmail.com>
2024-06-10 16:46:45 +0800

    Validating User Input & Showing an Error Modal via useImperativeHandle

---
+++ src/components/Modal.jsx
@@ -0,0 +1,26 @@
+import { useRef, forwardRef, useImperativeHandle } from 'react';
+import { createPortal } from 'react-dom';
+
+const Modal = forwardRef(function Modal({ children, buttonLabel }, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog ref={dialog}>
+      {children}
+      <form action='dialog'>
+        <button>{buttonLabel}</button>
+      </form>
+    </dialog>,
+    document.getElementById('modal-root')
+  );
+});
+
+export default Modal;
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,43 +1,59 @@
 import { useRef } from 'react';
 import Input from './Input';
+import Modal from './Modal';
 
 export default function NewProject({ onSubmit }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
 
   const handleSubmit = () => {
-    const data = {
+    if (
+      title.current.value.trim() === '' ||
+      description.current.value.trim() === '' ||
+      dueDate.current.value.trim() === ''
+    ) {
+      modal.current.open();
+      return;
+    }
+
+    onSubmit({
       title: title.current.value,
       description: description.current.value,
       dueDate: dueDate.current.value,
-    };
-
-    onSubmit(data);
+    });
   };
 
   return (
-    <div className='w-[35rem] mt-16'>
-      <menu className='flex items-center justify-end gap-4 my-4'>
-        <li>
-          <button className='text-stone-800 hover:text-stone-950'>
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={handleSubmit}
-            className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input ref={title} type='text' label='Title' />
-        <Input ref={description} label='Description' textarea />
-        <Input ref={dueDate} type='date' label='Due Date' />
+    <>
+      <Modal ref={modal} buttonLabel='Okay'>
+        <h2>Error</h2>
+        <p>Please fill up.</p>
+      </Modal>
+      <div className='w-[35rem] mt-16'>
+        <menu className='flex items-center justify-end gap-4 my-4'>
+          <li>
+            <button className='text-stone-800 hover:text-stone-950'>
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSubmit}
+              className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input ref={title} type='text' label='Title' />
+          <Input ref={description} label='Description' textarea />
+          <Input ref={dueDate} type='date' label='Due Date' />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
